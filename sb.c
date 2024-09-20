6a1fc10b45ebd6918e212522d9bcce5e6c190248
Samuel <morais123samuel@gmail.com>
2024-09-19 23:26:05 -0300

    Cria tela sobre

--- sb.c
+++ sb.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+//assinatura das funções
+void tela_sobre(void);
+
 //codigo principal
 
 int main (void){
-    printf("Programa SIG-Beer");
+    tela_sobre();
     return 0;
+}
+
+//funções
+    //tela sobre
+void tela_sobre(void){
+    printf("\n");
+    printf("|-------------------------------------------------------------------------|\n");
+    printf("|                           Sobre O Programa                              |\n");
+    printf("|-------------------------------------------------------------------------|\n");
+    printf("|  Este programa tem intuito de ser um sistema de assinatura de cervejas, |\n");
+    printf("|onde irá oferecer vários packs de cervejas artesenais de alta qualidade. |\n");
+    printf("|Mas sendo ainda um projeto didático para a disciplina de Programação do  |\n");
+    printf("|curso de Bacharelado de Sistemas de Informação da UFRN.                  |\n");
+    printf("|-------------------------------------------------------------------------|\n");
 }
