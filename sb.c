36bc02bdc6813e68f31536954673ff12bf485e73
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-09-20 18:08:10 -0300

    Cria Tela Equipe

--- sb.c
+++ sb.c
@@ -13,11 +13,13 @@
 //assinatura das funções
 void tela_principal(void);
 void tela_sobre(void);
+void tela_equipe(void);
 
 //codigo principal
 int main (void){
     tela_principal();
     tela_sobre();
+    tela_equipe();
     return 0;
 }
 
@@ -48,4 +50,26 @@ void tela_sobre(void){
     printf("║ Mas sendo ainda um projeto didático para a disciplina de Programação do    ║\n");
     printf("║ curso de Bacharelado de Sistemas de Informação da UFRN.                    ║\n");
     printf("╚════════════════════════════════════════════════════════════════════════════╝\n");
+}
+
+void tela_equipe(void){
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                       EQUIPE DE DESENVOLVIMENTO                      ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                   José Guilherme Silva de Araújo                     ║\n");
+    printf("║                   Email: guilherme531araujo@gmail.com                ║\n");
+    printf("║                   Redes Sociais: @oguiaraujo_o                       ║\n");
+    printf("║                   Git: https://github.com/oguiaraujo                 ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                   Samuel Morais de Araújo                            ║\n");
+    printf("║                   Email: morais123samuel@gmail.com                   ║\n");
+    printf("║                   Git: https://github.com/SamuelMorais45             ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
 }
