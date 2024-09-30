5b1fa81daa92ced7124a4e6644f9191dc8cbfda7
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-09-30 14:37:54 -0300

    Sincroniza o espaçamento de clientes.c com sb.c

--- clientes.c
+++ clientes.c
@@ -9,95 +9,95 @@
 #include "clientes.h"
 
 char tela_menu_clientes(void){
-  char op;
-  system("clear||cls");
-  printf("\n");
-  printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
-  printf("║                                                                      ║\n");
-  printf("║                            MENU CLIENTES                             ║\n");
-  printf("║                                                                      ║\n");
-  printf("╟──────────────────────────────────────────────────────────────────────╢\n");
-  printf("║                                                                      ║\n");
-  printf("║                     1. CADASTRAR NOVO CLIENTE                        ║\n");
-  printf("║                     2. PESQUISAR DADOS DO CLIENTE                    ║\n");
-  printf("║                     3. ATUALIZAR DADOS DO CLIENTE                    ║\n");
-  printf("║                     4. REMOVER CLIENTE DO SISTEMA                    ║\n");
-  printf("║                     0. VOLTAR                                        ║\n");
-  printf("║                                                                      ║\n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-  printf("                         Escolha uma opção: ");
-  scanf("%c", &op);
-  getchar();
-  printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
-  getchar();
-  return op;
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                            MENU CLIENTES                             ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                     1. CADASTRAR NOVO CLIENTE                        ║\n");
+    printf("║                     2. PESQUISAR DADOS DO CLIENTE                    ║\n");
+    printf("║                     3. ATUALIZAR DADOS DO CLIENTE                    ║\n");
+    printf("║                     4. REMOVER CLIENTE DO SISTEMA                    ║\n");
+    printf("║                     0. VOLTAR                                        ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    printf("                         Escolha uma opção: ");
+    scanf("%c", &op);
+    getchar();
+    printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
+    getchar();
+    return op;
 }
 
 void tela_cadastrar_cliente(void){
-  system("clear||cls");
-  printf("\n");
-  printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
-  printf("║                                                                      ║\n");
-  printf("║                          CADASTRAR CLIENTE                           ║\n");
-  printf("║                                                                      ║\n");
-  printf("╟──────────────────────────────────────────────────────────────────────╢\n");
-  printf("║                                                                      ║\n");
-  printf("║                      -> CPF (Apenas números):                        ║\n");
-  printf("║                      -> NOME COMPLETO:                               ║\n");
-  printf("║                      -> DATA DE NASCIMENTO (dd/mm/aaaa):             ║\n");
-  printf("║                      -> ENDEREÇO:                                    ║\n");
-  printf("║                      -> CELULAR (Apenas números):                    ║\n");
-  printf("║                      -> E-MAIL:                                      ║\n");
-  printf("║                                                                      ║\n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-  printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
-  getchar();
+    system("clear||cls");
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                          CADASTRAR CLIENTE                           ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                      -> CPF (Apenas números):                        ║\n");
+    printf("║                      -> NOME COMPLETO:                               ║\n");
+    printf("║                      -> DATA DE NASCIMENTO (dd/mm/aaaa):             ║\n");
+    printf("║                      -> ENDEREÇO:                                    ║\n");
+    printf("║                      -> CELULAR (Apenas números):                    ║\n");
+    printf("║                      -> E-MAIL:                                      ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
+    getchar();
 }
 
 void tela_pesquisar_cliente(void){
-  system("clear||cls");
-  printf("\n");
-  printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
-  printf("║                                                                      ║\n");
-  printf("║                          PESQUISAR CLIENTE                           ║\n");
-  printf("║                                                                      ║\n");
-  printf("╟──────────────────────────────────────────────────────────────────────╢\n");
-  printf("║                                                                      ║\n");
-  printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
-  printf("║                                                                      ║\n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-  printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
-  getchar();
+    system("clear||cls");
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                          PESQUISAR CLIENTE                           ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
+    getchar();
 }
 
 void tela_atualizar_cliente(void){
-  system("clear||cls");
-  printf("\n");
-  printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
-  printf("║                                                                      ║\n");
-  printf("║                          ATUALIZAR CLIENTE                           ║\n");
-  printf("║                                                                      ║\n");
-  printf("╟──────────────────────────────────────────────────────────────────────╢\n");
-  printf("║                                                                      ║\n");
-  printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
-  printf("║                                                                      ║\n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-  printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
-  getchar();
+    system("clear||cls");
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                          ATUALIZAR CLIENTE                           ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
+    getchar();
 }
 
 void tela_apagar_cliente(void){
-  system("clear||cls");
-  printf("\n");
-  printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
-  printf("║                                                                      ║\n");
-  printf("║                           APAGAR CLIENTE                             ║\n");
-  printf("║                                                                      ║\n");
-  printf("╟──────────────────────────────────────────────────────────────────────╢\n");
-  printf("║                                                                      ║\n");
-  printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
-  printf("║                                                                      ║\n");
-  printf("╚══════════════════════════════════════════════════════════════════════╝\n");
-  printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
-  getchar();
+    system("clear||cls");
+    printf("\n");
+    printf("╔══════════════════════════════-SIG-BEER-══════════════════════════════╗\n");
+    printf("║                                                                      ║\n");
+    printf("║                           APAGAR CLIENTE                             ║\n");
+    printf("║                                                                      ║\n");
+    printf("╟──────────────────────────────────────────────────────────────────────╢\n");
+    printf("║                                                                      ║\n");
+    printf("║                  -> INSIRA O CPF (Apenas números):                   ║\n");
+    printf("║                                                                      ║\n");
+    printf("╚══════════════════════════════════════════════════════════════════════╝\n");
+    printf("  ──────────────────Pressione <ENTER> para continuar──────────────────  \n");
+    getchar();
 }
