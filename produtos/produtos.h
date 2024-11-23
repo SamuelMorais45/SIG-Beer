e6170aea6296107bcb821b4cae50776afc15e288
Samuel <morais123samuel@gmail.com>
2024-11-22 21:39:27 -0300

    Cria struct para produtos

--- produtos/produtos.h
+++ produtos/produtos.h
@@ -3,6 +3,20 @@
 ║                  M Ó D U L O  P R O D U T O S                   ║
 ╚═════════════════════════════════════════════════════════════════╝
 */
+
+
+
+
+struct Produtos{
+    char nomeprod[22];
+    char teor[5];
+    char amargor;
+    char quant[6];
+    char id[12];
+
+
+
+};
 void modulo_produtos(void);
 char menu_produtos(void);
 void cadastrar_produtos(void);
