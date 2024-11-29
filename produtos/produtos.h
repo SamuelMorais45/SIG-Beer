78146bdf98690e9429e6b73f60d37b55caeb44e2
Nathan L. <97923367+coderlupus@users.noreply.github.com>
2024-11-29 13:19:27 -0300

    mudando o .h pra add as funções de manipular arquivos

--- produtos/produtos.h
+++ produtos/produtos.h
@@ -5,21 +5,21 @@
 */
 
 
-
-
-struct Produtos{
+struct Produtos {
     char nomeprod[22];
     char teor[5];
     char amargor;
     char quant[6];
     char id[12];
-
-
-
 };
+
 void modulo_produtos(void);
 char menu_produtos(void);
 void cadastrar_produtos(void);
 void pesquisar_produtos(void);
 void atualizar_produtos(void);
 void remover_produtos(void);
+
+// Novas funções para manipulação de arquivos
+void salvar_produtos_em_arquivo(void);
+void carregar_produtos_de_arquivo(void);
