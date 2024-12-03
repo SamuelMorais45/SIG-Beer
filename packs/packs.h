710d221c493a3ceee534d92c295af9622bb7997b
Nathan L. <97923367+coderlupus@users.noreply.github.com>
2024-12-03 13:32:32 -0300

    colocando as funções de salvamento em arquivos no packs.h

--- packs/packs.h
+++ packs/packs.h
@@ -4,17 +4,17 @@
 ╚══════════════════════════════════════════════════════════════╝
 */
 
-struct pack{
+struct pack {
     char idprods[22];
     char nomepack[42];
     char idpack[12];
 };
-
-
-
 
 void modulo_packs(void);
 char menu_packs(void);
 void cadastrar_packs(void);
 void pesquisar_packs(void);
 void remover_packs(void);
+void carregar_packs(void);
+void salvar_pack(struct pack *novo_pack);
+void remover_pack_por_id(const char *id);
