f911d70e565ee4469552c2e9edd037626dbc1eac
Nathan L. <97923367+coderlupus@users.noreply.github.com>
2024-12-05 23:07:41 -0300

    adicionando o status de ativo ou deletado no produtos.h

--- produtos/produtos.h
+++ produtos/produtos.h
@@ -5,12 +5,19 @@
 */
 
 
+/*
+╔═════════════════════════════════════════════════════════════════╗
+║                  M Ó D U L O  P R O D U T O S                   ║
+╚═════════════════════════════════════════════════════════════════╝
+*/
+
 struct Produtos {
     char nomeprod[22];
     char teor[5];
     char amargor;
     char quant[6];
     char id[12];
+    int status;  // 1: Ativo, 0: Excluído logicamente
 };
 
 void modulo_produtos(void);
@@ -18,8 +25,11 @@ char menu_produtos(void);
 void cadastrar_produtos(void);
 void pesquisar_produtos(void);
 void atualizar_produtos(void);
-void remover_produtos(void);
+void remover_produtos_logico(void);
+void remover_todos_produtos(void);
+void recuperar_produto(void);
 
-// Novas funções para manipulação de arquivos
+// Funções de manipulação de arquivos
 void salvar_produtos_em_arquivo(void);
 void carregar_produtos_de_arquivo(void);
+
