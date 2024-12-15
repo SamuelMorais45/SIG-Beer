90ff5c626b6db669641a16aeb55ca340d395b269
Samuel <morais123samuel@gmail.com>
2024-12-15 14:17:52 -0300

    Corrigindo erro de função não localizada no packs .h

--- packs/packs.h
+++ packs/packs.h
@@ -13,8 +13,7 @@ struct pack {
 
 void modulo_packs(void);
 char menu_packs(void);
-void cadastrar_packs(void);
-void pesquisar_packs(void);
-void remover_packs(void);
 void carregar_packs(void);
 void salvar_pack(struct pack *novo_pack);
+void remover_pack_por_id(const char *)
+int gerar_id_pack(void)
