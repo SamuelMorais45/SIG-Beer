aeb3ea3d04591aec74d81a18f4784ef60ed04491
Tomé Galileu Oliveira Arcanjo <tome.arcanjo.112@ufrn.edu.br>
2025-01-22 18:25:47 -0300

    Corrigindo erro de assinatura de produtos

--- produtos/produtos.h
+++ produtos/produtos.h
@@ -7,6 +7,7 @@
 #ifndef PRODUTOS_H
 #define PRODUTOS_H
 
+
 struct Produtos {
     char nomeprod[22];
     char teor[5];
@@ -29,5 +30,12 @@ void remover_produtos(void);
 // Funções de manipulação de arquivos
 void salvar_produtos_em_arquivo(void);
 void carregar_produtos_de_arquivo(void);
+
+// Array para armazenar os produtos
+#define MAX_PRODUTOS 1000
+extern struct Produtos lista_produtos[MAX_PRODUTOS];
+
+// Variável para armazenar o total de produtos registrados
+extern int total_produtos;
 
 #endif
